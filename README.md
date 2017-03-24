0ceabd5072990f53e51566c6010c0b33f88b028a
Rick Johnson <richardpatrickjohnson@gmail.com>
2017-03-24 09:26:47 -0400

    Moved remaining scraper.py code to extract links from html into methods

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # thresher
 
+A utility to harvest research content from research activity aggregated by SHARE from over 150 data sources.  With a given query to SHARE it supplies a list content links that are then crawled download links to content.  This content is then downloaded and linked to the corresponding JSON-LD record from SHARE.
+
+Install Dependendencies
+
+-Python 3.5
+
 Install Beautiful Soup according to instructions here:
 
 https://www.crummy.com/software/BeautifulSoup/bs4/doc/
--- thresher/scraper.py
+++ thresher/scraper.py
@@ -12,10 +12,6 @@ import furl
 
 from urllib.parse import urlparse
 
-#def get_links(source_page):
-    #A method to return a list of links found embedded in page
-
-#def get_download_links()
 #create function get mime_type; returns nil if unknown
 def get_mime_type(url):
     formatted_url = furl.furl(url)
@@ -35,59 +31,70 @@ def get_root_url(url):
 #import the library used to query a website
 from bs4 import BeautifulSoup
 
+#Returns a list of content url's and their mime/types from a given html page
+#That are not of mime-type 'text/html'
+def get_content_urls_from_html_page(url):
+    page_url = furl.furl(url)
+    response = requests.get(page_url.url)
+
+    #first check to see if content type is non html
+    mime_type = response.headers.get('content-type')
+    print("Root url: ", response.url)
+    #get url domain after redirect
+    root_url = get_root_url(response.url)
+    print("Root domain: ", root_url)
+    print("Root link mime_type: ", mime_type)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    #TODO ignore CSS files (add an ignore extension list)
+    #TODO add an ignore MIME/TYPE list (configurable parameter file)
+    print("-------------------------------------")
+
+    #create dictionary (hash) for links and content
+    content_links = {}
+
+    for a in soup.find_all('a', href=True):
+        href = a['href']
+        print("found href:", href)
+        if not href.startswith("http"):
+            full_url = root_url + href
+        else:
+            full_url = href
+
+        content_url = furl.furl(full_url)
+        mime_type = get_mime_type(full_url)
+        if mime_type is not None:
+            if "text/html" not in mime_type:
+                print("Found content link:", content_url, " MIME-TYPE: ", mime_type)
+                content_links[content_url.url] = mime_type
+                
+            else:
+                print("Found invalid URL:", content_url)
+    
+    return content_links
+
+#excavation example from researchgate
+url = "http://dx.doi.org/10.13140/RG.2.1.4495.1923"
+#get hash of url's and mime_types for non html content
+content_links = get_content_urls_from_html_page(url)
+print(content_links)
+url2 = "https://curate.nd.edu/show/ht24wh26g7d"
+content_links2 = get_content_urls_from_html_page(url2)
+print(content_links2)
 #put into method once working
 #url = "https://www.nsf.gov/awardsearch/showAward?AWD_ID=1560089"
 #url = "https://curate.nd.edu/show/ht24wh26g7d"
 #csv file from page above
 #url = "https://curate.nd.edu/downloads/hx11xd09r8t"
 
+#do share query
+#get links
 #start with DOI links from SHARE
 # one class get links
 #next class get objects
 #one class save files
 #one class create csv
-
-#excavation example from researchgate
-url = "http://dx.doi.org/10.13140/RG.2.1.4495.1923"
-
-page_url = furl.furl(url)
-#search_url.args['size'] = 3
-response = requests.get(page_url.url)
-
-#first check to see if content type is non html
-mime_type = response.headers.get('content-type')
-print("Root url: ", response.url)
-#get url domain after redirect
-root_url = get_root_url(response.url)
-print("Root domain: ", root_url)
-print("Root link mime_type: ", mime_type)
-
-soup = BeautifulSoup(response.text, "html.parser")
-
-#TODO: check mime type and detect if a file versus html content
-#print(response.headers.get('content-type'))
-#print(response.text)
-
-#TODO ignore CSS files (add an ignore extension list)
-#TODO add an ignore MIME/TYPE list (configurable parameter file)
-print("         ")
-print("----------Printing Soup--------------")
-for a in soup.find_all('a', href=True):
-    href = a['href']
-    if not href.startswith("http"):
-        full_url = root_url + href
-    else:
-        full_url = href
-
-    content_url = furl.furl(full_url)
-    mime_type = get_mime_type(full_url)
-    if mime_type is not None:
-        if "text/html" not in mime_type:
-            print("Found content link:", content_url, " MIME-TYPE: ", mime_type)
-    else:
-        print("Found invalid URL:", content_url)
-
-
 
 # get list of records from SHARE as JSON-LD
 # from record extract links
@@ -109,6 +116,7 @@ for a in soup.find_all('a', href=True):
 #Others look for other content
 
 #TODO incorporate full characterization and virus checking
+#TODO create unit tests
 
 #Challenges
 #Downloads hidden behind javascript or button
