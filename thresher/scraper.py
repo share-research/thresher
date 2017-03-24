b906ab0a98a87e393e8cd385e5f99f29ff505be3
Rick Johnson <richardpatrickjohnson@gmail.com>
2017-03-24 13:56:14 -0400

    Query results from SHARE sent to scraper

--- thresher/scraper.py
+++ thresher/scraper.py
@@ -6,83 +6,101 @@
 ## store metadata record
 ## fish for interest and next steps
 
-
 import requests
 import furl
 
 from urllib.parse import urlparse
 
-#create function get mime_type; returns nil if unknown
-def get_mime_type(url):
-    formatted_url = furl.furl(url)
-    try:
-        mime_response = requests.get(formatted_url.url)
-        mime_type = mime_response.headers.get('content-type')
-        return mime_type
-    except:
-        print ("Exception found for url: ", url)
-        None
-
-def get_root_url(url):
-    parsed_uri = urlparse(url)
-    root = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
-    return root
-
-#import the library used to query a website
+#import the library used to parse an html page
 from bs4 import BeautifulSoup
 
-#Returns a list of content url's and their mime/types from a given html page
-#That are not of mime-type 'text/html'
-def get_content_urls_from_html_page(url):
-    page_url = furl.furl(url)
-    response = requests.get(page_url.url)
+class Scraper:
+    #def __init__(self):
+        
 
-    #first check to see if content type is non html
-    mime_type = response.headers.get('content-type')
-    print("Root url: ", response.url)
-    #get url domain after redirect
-    root_url = get_root_url(response.url)
-    print("Root domain: ", root_url)
-    print("Root link mime_type: ", mime_type)
-
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    #TODO ignore CSS files (add an ignore extension list)
-    #TODO add an ignore MIME/TYPE list (configurable parameter file)
-    print("-------------------------------------")
-
-    #create dictionary (hash) for links and content
-    content_links = {}
-
-    for a in soup.find_all('a', href=True):
-        href = a['href']
-        print("found href:", href)
-        if not href.startswith("http"):
-            full_url = root_url + href
-        else:
-            full_url = href
-
-        content_url = furl.furl(full_url)
-        mime_type = get_mime_type(full_url)
-        if mime_type is not None:
-            if "text/html" not in mime_type:
-                print("Found content link:", content_url, " MIME-TYPE: ", mime_type)
-                content_links[content_url.url] = mime_type
-                
-            else:
-                print("Found invalid URL:", content_url)
+    #create function get mime_type; returns nil if unknown
+    def get_mime_type(self, url):
+        formatted_url = furl.furl(url)
+        try:
+            #use stream=True so it does not try to pre-download the content
+            mime_response = requests.get(formatted_url.url, stream=True)
+            mime_type = mime_response.headers.get('content-type')
+            return mime_type
+        except:
+            print ("Exception found for url: ", url)
+            None
+            
+    def get_root_url(self, url):
+        parsed_uri = urlparse(url)
+        root = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+        return root
     
-    return content_links
+
+
+    #Returns a list of content url's and their mime/types from a given html page
+    #That are not of mime-type 'text/html'
+    def get_content_urls_from_html_page(self, url):
+        print("starting get content links")
+        page_url = furl.furl(url)
+        response = requests.get(page_url.url, stream=True)
+
+        print("getting mime-type for first link")
+        #first check to see if content type is html
+        mime_type = response.headers.get('content-type')
+
+        #create dictionary (hash) for links and content
+        content_links = {}
+        
+        if "text/html" in mime_type:
+            #get url domain after redirect
+            root_url = self.get_root_url(response.url)
+
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            #TODO ignore CSS files (add an ignore extension list)
+            #TODO add an ignore MIME/TYPE list (configurable parameter file)
+
+            print("starting for loop")
+            for a in soup.find_all('a', href=True):
+                print("iteration")
+                href = a['href']
+
+                if not href.startswith("http"):
+                    full_url = root_url + href
+                else:
+                    full_url = href
+
+                #only if not already found on page
+                content_url = furl.furl(full_url)
+
+                if content_url.url not in content_links:
+                    mime_type = self.get_mime_type(full_url)
+                    if mime_type is not None:
+                        if "text/html" not in mime_type:
+                            print("Found content link:", content_url, " MIME-TYPE: ", mime_type)
+                            content_links[content_url.url] = mime_type
+                    else:
+                        print("Found invalid URL:", content_url)
+        else:
+            #just return first page if not html
+            content_links[page_url.url] = mime_type
+            
+        if not content_links:
+            #if no links found just return first page
+            content_links[page_url.url] = mime_type
+
+        return content_links
+
+#end def class Scraper
 
 #excavation example from researchgate
-url = "http://dx.doi.org/10.13140/RG.2.1.4495.1923"
+#url = "http://dx.doi.org/10.13140/RG.2.1.4495.1923"
 #get hash of url's and mime_types for non html content
-content_links = get_content_urls_from_html_page(url)
-print(content_links)
-url2 = "https://curate.nd.edu/show/ht24wh26g7d"
-content_links2 = get_content_urls_from_html_page(url2)
-print(content_links2)
-#put into method once working
+#content_links = get_content_urls_from_html_page(url)
+#print(content_links)
+#url2 = "https://curate.nd.edu/show/ht24wh26g7d"
+#content_links2 = get_content_urls_from_html_page(url2)
+#print(content_links2)
 #url = "https://www.nsf.gov/awardsearch/showAward?AWD_ID=1560089"
 #url = "https://curate.nd.edu/show/ht24wh26g7d"
 #csv file from page above
@@ -117,6 +135,8 @@ print(content_links2)
 
 #TODO incorporate full characterization and virus checking
 #TODO create unit tests
+
+#TODO develop Ruby support as well
 
 #Challenges
 #Downloads hidden behind javascript or button
